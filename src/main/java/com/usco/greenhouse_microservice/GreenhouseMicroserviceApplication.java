b1d62a54931fd649f9c807a5822824a908d8bdaf
MaitreStick <mastijaci99@gmail.com>
2024-11-05 23:09:36 -0500

    first commit

---
+++ src/main/java/com/usco/greenhouse_microservice/GreenhouseMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.usco.greenhouse_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhouseMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenhouseMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usco/greenhouse_microservice/GreenhouseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usco.greenhouse_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenhouseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
